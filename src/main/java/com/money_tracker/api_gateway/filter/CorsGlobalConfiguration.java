066027332133d5ef6c50b414c39d07d2a169ff59
Faizan Khan <faizankhan062002@gmail.com>
2025-01-25 09:24:14 +0530

    Create CorsGlobalConfiguration.java

---
+++ src/main/java/com/money_tracker/api_gateway/filter/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.money_tracker.api_gateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("http://localhost:5175"); // Allow all origins
+        corsConfig.addAllowedHeader("*"); // Allow all headers
+        corsConfig.addAllowedMethod("*"); // Allow all methods
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Apply to all paths
+
+        return new CorsWebFilter(source);
+    }
+}
